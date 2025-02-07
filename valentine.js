be469171256489e9a06c2bbe29697f85c80813d7
lavanyadeepthi <rs200391@rguktsklm.ac.in>
2025-02-07 19:09:41 +0530

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,10 @@
+function yes(){
+    
+    const pel=document.querySelector("p");
+    pel.innerHTML="Thankyou My Pote";
+}
+function no(){
+    
+    const pel=document.querySelector("p");
+    pel.innerHTML="There is no 'NO' You should be my Pote.";
+}
